cb3c7470dc65f0ac28540322fee73cab410bc75d
Minsu Cho <ncms0820@outlook.com>
2022-03-17 00:09:35 +0900

    initialized

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Searhc");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("delete video");
